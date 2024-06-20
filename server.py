e84f80bf64086110d81bdbbe52ce3bd40f7257fd
penguin <penguin0610@naver.com>
2024-06-20 22:43:16 +0900

    제발 다른 기기에서도 돌아가도록 해주세요ㅜㅜㅜㅜㅜ

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ import http.server
 import socketserver
 
 # 서버가 열 포트와 임의의 IP 주소를 지정
-HOST = '0.0.0.0'  # 모든 인터페이스에서 요청을 수락
-PORT = 8000
+HOST = '192.168.0.113'  # 모든 인터페이스에서 요청을 수락
+PORT = 8080
 
 Handler = http.server.SimpleHTTPRequestHandler
 
